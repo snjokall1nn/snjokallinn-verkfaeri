e4ffca3e24eb21ad07441ed08337a46163ce4d10
snjokall1nn <snjokallinn@snjokallinn.com>
2026-09-16 11:36:17 +0000

    Connect Snjókallinn Verkfæri to Apps Script

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// Paste the deployed Google Apps Script Web App URL here.
-// Example: window.SNJO_VERKFAERI_SCRIPT_URL = 'https://script.google.com/macros/s/ABC123/exec';
-window.SNJO_VERKFAERI_SCRIPT_URL = '';
+// Deployed Google Apps Script Web App URL.
+window.SNJO_VERKFAERI_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwycOL9LNTQ0R-5uC7PYqvn5AtBnNqV_SFNq3LOmLF4vWsMpal5fHP_XaOxRFGzBlFnhw/exec';
